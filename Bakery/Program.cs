d543d3a000e9ce42027d03dd1f2c4e6d3825251d
onurkaymak <onurkaymak@outlook.com>
2023-09-22 12:54:55 -0700

    Create greeting and menu for UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,21 +7,18 @@ namespace Bakery
   {
     static void Main()
     {
-      Console.WriteLine("*~**~**~**~**~**~**~**~**~**~**~**~**~**~**~*");
-      Console.WriteLine("                          ");
-      Console.WriteLine("Welcome to Pierre's Bakery");
-      Console.WriteLine("                          ");
-      Console.WriteLine("*---> Bread Prices <---*");
-      Console.WriteLine("                          ");
-      Console.WriteLine("Single loaf $5");
-      Console.WriteLine("Buy 2, get 1 free.");
-      Console.WriteLine("                          ");
-      Console.WriteLine("*---> Pastry Prices <---*");
-      Console.WriteLine("                          ");
-      Console.WriteLine("Single pastry $2");
-      Console.WriteLine("Buy 3, get 1 free.");
-      Console.WriteLine("                          ");
-
+      Console.WriteLine("**~**~**~**~**~**~**~**~**~**~**~**~**~***");
+      Console.WriteLine("*       Welcome to Pierre's Bakery       *");
+      Console.WriteLine("*                                        *");
+      Console.WriteLine("*       *---> Bread Prices <---*         *");
+      Console.WriteLine("*                                        *");
+      Console.WriteLine("*   Single loaf $5 | Buy 2, get 1 free!  *");
+      Console.WriteLine("*                                        *");
+      Console.WriteLine("*      *---> Pastry Prices <---*         *");
+      Console.WriteLine("*                                        *");
+      Console.WriteLine("* Single pastry $2 | Buy 3, get 1 free!  *");
+      Console.WriteLine("*                                        *");
+      Console.WriteLine("**~**~**~**~**~**~**~**~**~**~**~**~**~***");
     }
   }
 }
