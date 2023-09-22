c215166e1748336a39540522127d8482ee9b27fa
onurkaymak <onurkaymak@outlook.com>
2023-09-22 11:27:38 -0700

    pass CalcCost method test for Pastry class, add price of single pastry in Pastry class

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -17,5 +17,15 @@ namespace Bakery.Tests
       Assert.AreEqual(orderAmount, result);
     }
 
+    [TestMethod]
+    public void CalcCostMethod_CalculatesCostForSinglePastry_Cost()
+    {
+      int cost = 2;
+      int orderAmount = Pastry.NewOrder(1);
+      int result = Pastry.CalcCost();
+      Assert.AreEqual(cost, result);
+    }
+
+
   }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,10 +2,16 @@ namespace Bakery.Models
 {
   public class Pastry
   {
+    public static int PriceOfSinglePastry { get; set; } = 2;
     public static int OrderAmount { get; set; }
     public static int NewOrder(int amount)
     {
       return OrderAmount = amount;
+    }
+
+    public static int CalcCost()
+    {
+      return OrderAmount * PriceOfSinglePastry;
     }
   }
 }
