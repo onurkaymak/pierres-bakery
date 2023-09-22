838e79d3b295d9bb2570e9d2afb23b94c83a5a80
onurkaymak <onurkaymak@outlook.com>
2023-09-22 08:36:15 -0700

    Add boilerplate project template

---
+++ Bakery/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("*~**~**~**~**~**~**~**~**~**~**~**~**~**~**~*");
+      Console.WriteLine("                          ");
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine("                          ");
+      Console.WriteLine("*---> Bread Prices <---*");
+      Console.WriteLine("                          ");
+      Console.WriteLine("Single loaf $5");
+      Console.WriteLine("Buy 2, get 1 free.");
+      Console.WriteLine("                          ");
+      Console.WriteLine("*---> Pastry Prices <---*");
+      Console.WriteLine("                          ");
+      Console.WriteLine("Single pastry $2");
+      Console.WriteLine("Buy 3, get 1 free.");
+      Console.WriteLine("                          ");
+
+    }
+  }
+}
